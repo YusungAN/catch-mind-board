3302c9589e408ba743ca4bd8754ce701979738c8
YusungAN <52990109+YusungAN@users.noreply.github.com>
2020-12-14 17:59:02 +0900

    Delete config.js

--- catch-mind-backend/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    secret: '비밀임',
-    mysqlconfig: {
-        host     : 'remotemysql.com',
-        user     : '비밀임',
-        password : '비밀임',
-        database : '비밀임'
-    }
-};
